e4974a124b5224a87b2c894cade7a39b13dd2f50
mizok <mizok.huang@neutec.com.tw>
2023-12-01 16:06:46 +0800

    fix: 修正esbuild config/postinstall設置

--- esbuild.ts
+++ esbuild.ts
@@ -1,12 +1,23 @@
 // esbuild.config.ts
 import { BuildOptions, build } from 'esbuild'
+import copyfiles from 'copyfiles'
+import { rimraf } from 'rimraf'
 
 const config: BuildOptions = {
   entryPoints: ['src/main.ts'],
   bundle: true,
   minify: true,
   outfile: 'dist/main.js',
+  platform: 'node',
   tsconfig: './tsconfig.json',
 }
 
 build(config)
+  .then(() => {
+    return rimraf('./dist/locales')
+  })
+  .then(() => {
+    copyfiles(['locales/*', 'dist'], { all: false }, () => {
+      console.log('Locales files copied successfully!')
+    })
+  })
