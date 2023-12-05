1fceafcba57f75e60b87e4a141f87a27bc896ea8
mizok <mizok.huang@neutec.com.tw>
2023-12-05 19:56:03 +0800

    fix: 補上postinstall設置

--- esbuild.ts
+++ esbuild.ts
@@ -2,6 +2,8 @@
 import { BuildOptions, build } from 'esbuild'
 import copyfiles from 'copyfiles'
 import { rimraf } from 'rimraf'
+import * as os from 'os'
+import { exec } from 'child_process'
 
 const config: BuildOptions = {
   entryPoints: ['src/main.ts'],
@@ -14,10 +16,27 @@ const config: BuildOptions = {
 
 build(config)
   .then(() => {
-    return rimraf('./dist/locales')
+    return Promise.all([
+      rimraf('./dist/locales'),
+      rimraf('./dist/package.json'),
+      rimraf('./dist/static'),
+    ])
   })
   .then(() => {
+    const platform = os.platform()
     copyfiles(['locales/*', 'dist'], { all: false }, () => {
       console.log('Locales files copied successfully!')
     })
+    copyfiles(['static/*', 'dist'], { all: false }, () => {
+      console.log('static files copied successfully!')
+    })
+    if (platform === 'linux' || platform === 'darwin') {
+      try {
+        exec('chmod +x ./dist/main.js', (err, stdout, stderr) => {
+          if (err) console.error(err)
+        })
+      } catch (err) {
+        console.error(err)
+      }
+    }
   })
